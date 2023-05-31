5264aeb421bc3c41667c1e1113c3819045a2f8fd
RonaldsonBellande <ronaldsonbellande@gmail.com>
2023-05-31 13:19:18 -0400

    recal

--- push.sh
+++ push.sh
@@ -1,1 +1,65 @@
-git add .; git commit -am "progress"; git push
+#!/bin/bash
+
+# Get the current directory
+current_dir=$(pwd)
+
+# Read the remote repository URL from .git/config
+remote_repo_url=$(git -C "$current_dir" config --get remote.origin.url)
+
+# Create a temporary directory for cloning the repository
+temp_dir=$(mktemp -d)
+
+# Clone the repository into the temporary directory without using local references
+git clone --no-local "$current_dir" "$temp_dir"
+
+# Switch to the temporary directory
+cd "$temp_dir"
+
+# Create a temporary file to store the file list
+tmp_file=$(mktemp)
+
+# Function to check if a file or folder exists in the repository
+file_exists_in_repo() {
+  local file_path="$1"
+
+  # Check if the file or folder exists in the repository
+  git ls-tree --name-only -r HEAD | grep -Fxq "$file_path"
+}
+
+# Function to process the files and folders in each commit
+process_commit_files() {
+  local commit="$1"
+
+  # Get the list of files and folders in the commit (including subfolders)
+  git ls-tree --name-only -r "$commit" >> "$tmp_file"
+
+  # Process each file or folder in the commit
+  while IFS= read -r line
+  do
+    # Check if the file or folder exists in the current push
+    if file_exists_in_repo "$line"; then
+      echo "Keeping: $line"
+    else
+      echo "Deleting: $line"
+      git filter-repo --path "$line" --invert-paths
+    fi
+  done < "$tmp_file"
+
+  # Clear the temporary file
+  > "$tmp_file"
+}
+
+# Iterate over each commit in the repository
+git rev-list --all | while IFS= read -r commit
+do
+  process_commit_files "$commit"
+done
+
+# Push the filtered changes to the original repository
+git remote add origin "$remote_repo_url"
+git push --force --set-upstream origin main
+
+# Clean up temporary files and directories
+cd "$current_dir"
+rm -rf "$temp_dir"
+rm "$tmp_file"
